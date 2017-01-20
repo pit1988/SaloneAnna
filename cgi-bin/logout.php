96426cc13f14841083795b8061aff7e14a09c41f
pit1988 <gabelli29@hotmail.com>
2017-01-20 17:03:50 +0100

    Spostati file di utilità. Se si cambia il nome della cartella, è da cambiare anche dentro Libreria sulla parte dell'header.

--- cgi-bin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_unset(); //questa funzione elimina le variabili contenute nella sessione
-session_destroy(); //questa funzione elimina la sessione
-header('location:../index.php'); //da controllare che indirizzi correttamente la pagina
-?>
