25e24842c8b2758225038fbe869bce33cd475324
pit1988 <gabelli29@hotmail.com>
2017-01-26 22:31:52 +0100

    concluse pagine inserimento appuntamento

--- public_html/NuovoAppuntamento.php
+++ public_html/NuovoAppuntamento.php
@@ -12,36 +12,34 @@ if (!isset($_SESSION['username'])) {
     require 'library.php';
     require 'utils/DBlibrary.php';
     
-    $title = "Nuovo appuntamento: Salone Anna";
+    $title      = "Nuovo appuntamento: Salone Anna";
     $title_meta = "Nuovo appuntamento: Salone Anna";
-    $descr = "";
-    $keywords = "Nuovo, Appuntamento, Parrucchiere, Montecchio, Vicenza, Taglio, Colorazioni, Donna";
+    $descr      = "";
+    $keywords   = "Nuovo, Appuntamento, Parrucchiere, Montecchio, Vicenza, Taglio, Colorazioni, Donna";
     
     page_start($title, $title_meta, $descr, $keywords, '');
     $rif = '<a href="index.php" xml:lang="en">Home</a> / <a href="Appuntamenti.php">Appuntamenti</a> / <strong>Nuovo Appuntamento</strong>';
     insert_header($rif, 4, true);
     content_begin();
     //aggiungere tabindex; modificare input type radio in modo da lasciare 
-    $str1= '<form action="conferma_appuntamento.php" onsubmit="return true;" method="post">
+    $str1 = '<form action="conferma_appuntamento.php" onsubmit="return true;" method="post">
                 <ul>
                     <li>
                         <p> 
                             <label for="TipoAppuntamento">Tipo appuntamento:</label>
-                            <input type="radio" name="TipoAppuntamento" value="shampoo" />Shampoo
-							<input type="radio" name="TipoAppuntamento" value="taglio" />Taglio
-							<input type="radio" name="TipoAppuntamento" value="piega e phon" />Piega e Phon
-							<input type="radio" name="TipoAppuntamento" value="piega e casco" />Piega e Casco
-							<input type="radio" name="TipoAppuntamento" value="ondulazione" />Ondulazione
-							<input type="radio" name="TipoAppuntamento" value="colore" />Colore
-							<input type="radio" name="TipoAppuntamento" value="riflessante" />Riflessante
-							<input type="radio" name="TipoAppuntamento" value="decolorazione" />Decolorazione
-							<input type="radio" name="TipoAppuntamento" value="meches" />Meches
-							<input type="radio" name="TipoAppuntamento" value="trattamenti" />Trattamenti
 ';
-
-	$str2="";
-$str3='
-                        </p>
+    
+    $str2        = "";
+    $qry         = "SELECT CodTipoAppuntamento, NomeTipo FROM TipoAppuntamento";
+    $conn        = dbconnect();
+    $result      = mysqli_query($conn, $qry);
+    $number_rows = mysqli_num_rows($result);
+    if ($number_rows > 1) {
+        while ($row = mysqli_fetch_row($result)) {
+            $str2 .= '<input type="radio" name="TipoAppuntamento" value="' . $row[0] . '" />' . $row[1]."\n";
+        }
+    }
+    $str3 = '</p>
                     </li>
                     <li>
                         <p>
@@ -66,25 +64,15 @@ $str3='
                         </p>
                     </li>
                     <li>
-                        <label for="costo">Costo</label>
-                         <input type="text" name="costo" id="costo" tabindex="103" />
-                    </li>
-                        <li>
-                        <label for="sconto">Sconto</label>
-                         <input type="text" name="sconto" id="sconto" tabindex="103" />
-                    </li>
-                    <li>
                         <input class="btn btn-submit" type="submit" name="submit" value="Invia" tabindex="105"/>
                         <input type="reset" value="cancella"
                         <span id="errors"></span>
                     </li>
-                    <li>
-                        <div class="divider"></div>
-                    </li>
+                    
                 </ul>
             </form>
 ';
-	echo $str1.$str2.$str3;
+    echo $str1 . $str2 . $str3;
     content_end();
     page_end();
 }
--- public_html/conferma_appuntamento.php
+++ public_html/conferma_appuntamento.php
@@ -1,95 +1,119 @@
-<?php
-include 'DBlibrary.php';
-include 'library.php';
-if (isset($_POST['submit'])) {
-    if (!isset($_POST['TipoAppuntamento']) OR !isset($_POST['first_name']) OR !isset($_POST['last_name']) OR !isset($_POST['data']) OR !isset($_POST['orario'])) { //OR !isset($_POST['costo']) OR !isset($_POST['sconto'])) {
-        $err = "Almeno uno dei parametri non è stato inserito correttamente";
-    } else {
-        $sub     = $_POST['submit'];
-        $tipo    = $_POST['TipoAppuntamento'];
-        $nome    = $_POST['first_name'];
-        $cognome = $_POST['last_name'];
-        $date    = $_POST['data'];
-        $time    = $_POST['orario'];
-/*        $costo   = $_POST['costo'];
-        $sconto  = $_POST['sconto'];*/
-        
-        if (strlen($tipo) == 0 OR strlen($nome) == 0 OR strlen($cognome) == 0 OR strlen($date) == 0 OR strlen($time) == 0) //OR strlen($costo) == 0 OR strlen($sconto) == 0)
-            $err = "Almeno uno dei parametri non è stato inserito correttamente";
-        else {
-            $orario     = date_create_from_format('H:i', $time);
-            $data       = date_format(date_create_from_format('d/m/Y', $date), 'Y-m-d');
-            $dataora = date('Y-m-d H:i:s', strtotime("$data $orario"));
-            echo $dataora;
-            
-            $conn        = dbConnect();
-            $queryc      = ("select * from Clienti c where c.Nome = $nome and c.Cognome = $cognome");
-            $result      = mysql_query(queryc);
-            $number_rows = mysql_num_rows($result);
-            if ($number_rows > 1) {
-                echo "<p>Più clienti hanno si chiamano ".$nome." ".$cognome.", scegline uno:</p>";
-                $number_cols = mysql_num_fields($result);
-                form_start("POST", "confermaAppuntamento.php");
-                $th = '<table id="ProdottiMagazzino" summary="Prodotti in magazzino">
-            <caption>Prodotti modificabili</caption>
-            <thead>
-                <tr>
-                    <th scope="col">Codice Cliente</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Cognome</th>
-                    <th scope="col">Telefono</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Data di nascita</th>
-                </tr>
-            </thead>
-            <tbody>
-            ';
-                $tb = "";
-                //corpo tabella
-                while ($row = mysqli_fetch_row($result)) {
-                    
-                    $tb .= "<tr>\n";
-                    for ($i = 0; $i < $number_cols + 1; $i++) {
-                        $tb .= "<td>";
-                        if (!isset($row[$i]))
-                            $tb .= " ";
-                        if ($i == 0)
-                            $tb .= "<input type=\"radio\" name=\"CodCliente\" value= \"" . $row[$i] . "\"\/>";
-                        else {
-                            $tb .= $row[$i - 1];
-                        }
-                        
-                        $tb .= "</td>\n";
-                    }
-                    $tb .= "</tr>\n";
-                }
-                
-                $tf       = "</tbody></table>";
-                $to_print = $th . $tb . $tf;
-                echo $to_print;
-                echo "<input type='submit' name='submit' value='Procedi'>";
-                echo "<input type='reset' value='Cancella'>";
-                // echo"</fieldset>";
-                echo "</form>";
-            } //fine n_righe>1
-            if(!isset($_POST['CodCliente'])) {
-            	$CodClienteA = mysqli_query($conn, "SELECT RitCod('$nome', '$cognome')") or die("Query fallita " . mysqli_error($conn));
-            	$CodCliente = mysqli_fetch_row($CodClienteA);
-            }
-            else
-            	$CodCliente=$_POST['CodCliente'];
 
-            if (isset($submit)) {
-                $query = "INSERT INTO Appuntamenti (CodCliente ,  DataOra ,  CodTipoAppuntamento) values
-	      	('$CodCliente', '$dataora', '$tipo')";
+<?php
+
+session_start();
+session_regenerate_id(TRUE);
+
+// Controllo accesso
+if (!isset($_SESSION['username'])) {
+    header('location:index.php');
+    exit;
+} else {
+    if (!isset($_POST['submit'])) {
+        header('loation:NuovoAppuntamento.php');
+    } else {
+        require 'library.php';
+        require 'utils/DBlibrary.php';
+        
+        $title = "Nuovo appuntamento: Salone Anna";
+        $title_meta = "Nuovo appuntamento: Salone Anna";
+        $descr = "";
+        $keywords = "Nuovo, Appuntamento, Parrucchiere, Montecchio, Vicenza, Taglio, Colorazioni, Donna";
+        
+        page_start($title, $title_meta, $descr, $keywords, '');
+        $rif = '<a href="index.php" xml:lang="en">Home</a> / <a href="Appuntamenti.php">Appuntamenti</a> / <strong>Nuovo Appuntamento</strong>';
+        insert_header($rif, 4, true);
+        content_begin();
+        
+        if (!isset($_POST['TipoAppuntamento']) OR !isset($_POST['first_name']) OR !isset($_POST['last_name']) OR !isset($_POST['data']) OR !isset($_POST['orario'])) { //OR !isset($_POST['costo']) OR !isset($_POST['sconto'])) {
+            $err = "Almeno uno dei parametri non è stato inserito correttamente";
+        } else {
+            $sub = $_POST['submit'];
+            $tipo = $_POST['TipoAppuntamento'];
+            $nome = $_POST['first_name'];
+            $cognome = $_POST['last_name'];
+            $date = $_POST['data'];
+            $time = $_POST['orario'];
+            
+            if (strlen($tipo) == 0 OR strlen($nome) == 0 OR strlen($cognome) == 0 OR strlen($date) == 0 OR strlen($time) == 0) //OR strlen($costo) == 0 OR strlen($sconto) == 0)
+                $err = "Almeno uno dei parametri non è stato inserito correttamente";
+            else {
+                $orario = $time.":00";
+                $data = date_format(date_create_from_format('d/m/Y', $date), 'Y-m-d');
+                $dataora = date('Y-m-d H:i:s', strtotime("$data $orario"));
                 
+                
+                $conn = dbConnect();
+                $queryc = "SELECT * FROM Clienti c WHERE c.Nome = '$nome' AND c.Cognome = '$cognome'";
+                
+                $result = mysqli_query($conn, $queryc);
+                $number_rows = mysqli_num_rows($result);
+                if ($number_rows > 1) {
+                    echo "<p>Più clienti hanno si chiamano " . $nome . " " . $cognome . ", scegline uno:</p>";
+                    $number_cols = mysqli_num_fields($result);
+                    form_start("POST", "confermaAppuntamento.php");
+                    $th = '<table id="ProdottiMagazzino" summary="Prodotti in magazzino">
+	            <caption>Prodotti modificabili</caption>
+	            <thead>
+	                <tr>
+	                    <th scope="col">Codice Cliente</th>
+	                    <th scope="col">Nome</th>
+	                    <th scope="col">Cognome</th>
+	                    <th scope="col">Telefono</th>
+	                    <th scope="col">Email</th>
+	                    <th scope="col">Data di nascita</th>
+	                </tr>
+	            </thead>
+	            <tbody>
+	            ';
+                    $tb = "";
+                    //corpo tabella
+                    while ($row = mysqli_fetch_row($result)) {
+                        
+                        $tb .= "<tr>\n";
+                        for ($i = 0; $i < $number_cols + 1; $i++) {
+                            $tb .= "<td>";
+                            if (!isset($row[$i]))
+                                $tb .= " ";
+                            if ($i == 0)
+                                $tb .= "<input type=\"radio\" name=\"CodCliente\" value= \"" . $row[$i] . "\"\/>";
+                            else {
+                                $tb .= $row[$i - 1];
+                            }
+                            
+                            $tb .= "</td>\n";
+                        }
+                        $tb .= "</tr>\n";
+                    }
+                    
+                    $tf = "</tbody></table>";
+                    $to_print = $th . $tb . $tf;
+                    echo $to_print;
+                    echo "<input type='submit' name='submit' value='Procedi'>";
+                    echo "<input type='reset' value='Cancella'>";
+                    // echo"</fieldset>";
+                    echo "</form>";
+                } //fine n_righe>1
+                if (!isset($_POST['CodCliente'])) {
+                	$query="SELECT CodCliente FROM Clienti c WHERE c.Nome = '$nome' AND c.Cognome = '$cognome'";
+                    $CodClienteA = mysqli_query($conn, $query); //or die("Query fallita " . mysqli_error($conn));
+                    $CodCliente = mysqli_fetch_row($CodClienteA)[0];
+                } else
+                    $CodCliente = $_POST['CodCliente'];
+                
+                $query = "INSERT INTO Appuntamenti (CodCliente ,  DataOra ,  CodTipoAppuntamento) VALUES
+		      	('$CodCliente', '$dataora', '$tipo')";
+		      	
                 $ok = mysqli_query($conn, $query);
-    			if($ok)                
-                	echo "<b>Appuntamento di $nome inserito correttamente il $date alle $time</b><br>";
+                if ($ok)
+                    echo "<b>Appuntamento di $nome inserito correttamente il $date alle $time</b><br>";
                 
+                mysqli_close($conn);
             }
-            mysqli_close($conn);
+            content_end();
+            page_end();
         }
     }
 }
+
 ?>
