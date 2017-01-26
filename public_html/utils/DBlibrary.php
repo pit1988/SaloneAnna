4181777dad968c77525acdf220f4360037e965fc
Cenze94 <andrea.grendene94@gmail.com>
2017-01-26 17:29:30 +0100

    update DB e funzioni interrogazione DB

--- public_html/utils/DBlibrary.php
+++ public_html/utils/DBlibrary.php
@@ -11,15 +11,42 @@ function dbconnect() {
 };
 
 class Messaggio { //classe che rappresenta un messaggio
+	public $contenuto;
+	public $data;
+	public $ora;
+	public $daLeggere;
+	public $email;
+	public $nome;
+	public $cognome;
 	
+	function __construct($contenuto, $data, $ora, $daLeggere, $email, $nome, $cognome) {
+		$this->contenuto = $contenuto;
+		$this->data = $data;
+		$this->ora = $ora;
+		$this->daLeggere = $daLeggere;
+		$this->email = $email;
+		$this->nome = $nome;
+		$this->cognome = $cognome;
+	}
 }
 
 function listaMessaggi() { //i messaggi verranno già ordinati dal più recente al più vecchio
 	$conn = dbconnect();
-	$query = '';
+	$query = 'SELECT Contenuto, DataOra, ToRead, Email, Nome, Cognome
+	FROM Appuntamenti JOIN Clienti ON Appuntamenti.CodCliente = Clienti.CodCliente
+	ORDER BY DataOra DESC';
 	$result = $conn->query($query);
-	if(!result) {$err = "Errore nella query: ".$conn->error.".";} //intanto segnalo così il caso, è da eliminare se l'errore viene gestito in locale
-	else {return $result;}
-	$conn->close();
+	if(!result) {$err = "Errore nella query: ".$conn->error."."; $conn->close();} //intanto segnalo così il caso, è da eliminare se l'errore viene gestito in locale
+	else {
+		$messaggi = array();
+		while($messaggio = mysqli_fetch_assoc($result)) {
+			$time = strtotime($messaggio['DataOra']);
+			$data = date("d/m/Y", $time); //formato del tipo 05/01/2017
+			$ora = date("H:i", $time); //formato del tipo 23:46
+			array_push($messaggi, new Messaggio($messaggio['Contenuto'], $data, $ora, $messaggio['toRead'], $messaggio['Email'], $messaggio['Nome'], $messaggio['Cognome']));
+		}
+		$conn->close();
+		return $messaggi; //è un array di Messaggi
+	}
 }
 ?>
