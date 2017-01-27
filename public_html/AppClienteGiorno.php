dbc12810dccf0eb7e031dab0695ab0c0eb0f23fc
pit1988 <gabelli29@hotmail.com>
2017-01-27 13:22:32 +0100

    Conclusa pagina

--- public_html/AppClienteGiorno.php
+++ public_html/AppClienteGiorno.php
@@ -5,113 +5,100 @@ session_regenerate_id(TRUE);
 if (!isset($_SESSION['username'])) {
     header('location:index.php');
     exit;
-} elseif (!isset($_POST['submit']) OR !isset($_POST['cli']) OR !isset($_POST['data'])) {
-    $err = "<p>Problemi di connessione, <a href=\"RicercaAppuntamenti.php\"> segui il link per tornare alla pagina di ricerca</a>";
 } else {
-
-    require 'library.php';
-    require 'utils/DBlibrary.php';
+    require_once 'library.php';
+    require_once 'utils/DBlibrary.php';
+    if (!isset($_POST['submit']) OR (!isset($_POST['cli']) AND !isset($_POST['data']))) {
+        $err = "<p>Problemi di connessione, potresti non aver selezionato alcuna casella di ricerca. Segui il link per tornare alla <a href=\"RicercaAppuntamenti.php\">pagina di ricerca</a></p>";
+    } else {
+        $s_client = isset($_POST["cli"]) ? true : false;
+        $s_data   = isset($_POST["data"]) ? true : false;
+        
+        if (($s_client == true && (!isset($_POST['first_name']) OR !isset($_POST['last_name']))) OR ($s_data == true && (!isset($_POST['data'])))) { //OR !isset($_POST['costo']) OR !isset($_POST['sconto'])) {
+            $err = "Almeno uno dei parametri non è stato inserito correttamente";
+        } else {
+            $sub = $_POST['submit'];
+            
+            $query = "SELECT c.Nome, c.Cognome, a.DataOra	FROM Clienti c JOIN Appuntamenti a";
+            
+            if ($s_client == true) {
+                $nome    = $_POST['first_name'];
+                $cognome = $_POST['last_name'];
+                
+                $query .= "  WHERE c.Nome='$nome' AND c.Cognome='$cognome'";
+            }
+            
+            if ($s_data == true) {
+                if ($s_client == true)
+                    $query .= " AND ";
+                else
+                    $query .= " WHERE ";
+                $date = $_POST['date'];
+                $where .= "DATE(a.DataOra)=\"'$app'\"";
+                if (isset($_POST['orario'])) {
+                    $hh = substr($_POST['orario'], 0, 2);
+                    $where .= " AND HOUR(a.DataOra)>=\"'$hh.'\"";
+                }
+            }
+            
+            $conn  = dbConnect();
+            $query = $query;
+            
+            $result = $conn->query($query);
+            
+            $num_rows = mysqli_num_rows($result);
+            $ris      = "";
+            if (!$num_rows)
+                $ris = "<p>La ricerca non ha prodotto risultati</p>";
+            else {
+                
+                $number_cols = mysqli_num_fields($result);
+                
+                $ris .= "<p><strong>Storico:</strong></p>";
+                $ris .= '<table id="ProdottiMagazzino" summary="Prodotti in magazzino">
+            <caption>Prodotti modificabili</caption>
+            <thead>
+                <tr>' . "\n";
+                for ($i = 0; $i < $number_cols; $i++) {
+                    // echo "<th>" . mysql_field_name ($result, $i). "</th>\n";
+                    $ris .= "<th>" . (mysqli_fetch_field_direct($result, $i)->name) . "</th>\n";
+                }
+                $ris .= "</tr>\n</thead>\n<tbody>\n";
+                
+                while ($row = mysqli_fetch_row($result)) {
+                    $ris .= "<tr>\n";
+                    
+                    for ($i = 0; $i < $number_cols; $i++) {
+                        $ris .= "<td>";
+                        if (!isset($row[$i])) {
+                            $ris .= "NULL";
+                        } else {
+                            $ris .= $row[$i];
+                        }
+                        $ris .= "</td>\n";
+                    }
+                    $ris .= "</td>\n";
+                }
+                
+                $ris .= "</tbody></table>";
+            }
+            mysqli_close($conn);
+        }
+    }
     $title      = "Ricerca Appuntamento: Salone Anna";
     $title_meta = "Ricerca Appuntamento: Salone Anna";
     $descr      = "";
     $keywords   = "Appuntamento, Ricerca, Parrucchiere, Montecchio, Vicenza, Taglio, Colorazioni, Donna";
     page_start($title, $title_meta, $descr, $keywords, '');
-    $rif = '<a href="index.php" xml:lang="en">Home</a> / <a href="Appuntamenti.php">Appuntamenti</a> / <a href="RicercaAppuntamento.php">Ricerca Appuntamento</a> / <strong>Risultati</strong>';
+    $rif = '<a href="index.php" xml:lang="en">Home</a> / <a href="Appuntamenti.php">Appuntamenti</a> / <a href="RicercaAppuntamenti.php">Ricerca Appuntamento</a> / <strong>Risultati</strong>';
     insert_header($rif, 4, true);
     content_begin();
-
-	include ("utils/DBlibrary.php");
-	$conn = dbconnect();
-
-	$s_client = $_POST['cli'];
-	$s_data = $_POST['data'];
-	echo $s_client.$s_data;
-/*
-	if (($s_client==true && (!isset($_POST['first_name']) OR !isset($_POST['last_name']))) OR ($s_data==true && (!isset($_POST['data'])))) { //OR !isset($_POST['costo']) OR !isset($_POST['sconto'])) {
-            $err = "Almeno uno dei parametri non è stato inserito correttamente";
-        } else {
-            $sub = $_POST['submit'];
-            $nome = $_POST['first_name'];
-            $cognome = $_POST['last_name'];
-            $date = $_POST['date'];
-            $time = $_POST['orario'];
-            
-            if (strlen($nome) == 0 OR strlen($cognome) == 0 OR strlen($date) == 0 OR strlen($time) == 0) //OR strlen($costo) == 0 OR strlen($sconto) == 0)
-                $err = "Almeno uno dei parametri non è stato inserito correttamente";
-            else {
-                $orario = $time.":00";
-                $data = date_format(date_create_from_format('d/m/Y', $date), 'Y-m-d');
-                $dataora = date('Y-m-d H:i:s', strtotime("$data $orario"));
-                
-                
-                $conn = dbConnect();
-
-	$data=isset($_POST["data"])?$_POST["data"]:"";
-	$cliente=isset($_POST["cli"])?$_POST["cli"]:"";
-
-
-	if( !$data && !$cliente ) {
-		echo "<b>Devi selezionare almeno una delle due ricerche!</b><br>";
-		header('Location: RicercaAppuntamenti.php'); 
-	};
-
-	$query="
-	SELECT c.Nome, c.Cognome, a.DataOra
-	FROM Clienti c JOIN Appuntamenti a";
-
-	$where=" WHERE TRUE";
-
-	if($data){
-		$where .= " AND DATE(a.DataOra)=\"'$app'\"";
-		if($hh) $where .=" AND HOUR(a.DataOra)>=\"'$hh.'\"";
-	};
-	if($cliente){
-		if(!$nome || !$cognome) echo "La ricerca per cliente non e' andata a buon fine per mancanza di campi <br>";
-		else $where .=" AND c.Nome=\"'$nome'\"AND c.Cognome=\"'$cognome'\"";
-	};
-
-	$query= $query . $where;
-			
-	$result = $conn->query($query);
-
-	$number_cols = mysqli_num_fields($result);
-
-	echo "<b>Storico:</b>";
-	echo "<table border = 1>\n";
-	echo "<tr align=center>\n";
-
-	for($i=0; $i<$number_cols; $i++)
-		{
-			// echo "<th>" . mysql_field_name ($result, $i). "</th>\n";
-			echo "<th>" . mysqli_field_seek ($result, $i). "</th>\n";
-		}
-	echo "</tr>\n";
-
-	//intestazione tabella
-
-	//corpo tabella
-	while ($row = mysqli_fetch_row($result))
-	{
-		echo "<tr align=left>\n";
-
-		for ($i=0; $i<$number_cols; $i++) {
-			echo "<td>";
-			if(!isset($row[$i]))
-			  {echo "NULL";}
-			else
-			  {echo $row[$i];}
-			echo "</td>\n";
-		}
-	echo "</td>\n";
-	}
-
-	echo "</table>";
-
-	mysqli_close($conn);
-}
-	    
-} */
-content_end();
+    if (isset($err))
+        echo $err;
+    if (isset($ris))
+        echo $ris;
+    echo "<p>Segui il link per tornare alla <a href=\"RicercaAppuntamenti.php\">pagina di ricerca</a></p>";
+    content_end();
     page_end();
 }
 ?>
