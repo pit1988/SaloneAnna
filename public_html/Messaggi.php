1e6e6de032c0ab68853d1b7fdd079f42a69631fa
pit1988 <gabelli29@hotmail.com>
2017-01-27 18:52:11 +0100

    fatte pagine visione messaggi

---
+++ public_html/Messaggi.php
@@ -0,0 +1,57 @@
+
+<?php
+
+session_start();
+session_regenerate_id(TRUE);
+
+// Controllo accesso
+if (!isset($_SESSION['username'] ) )
+{
+	header('location:index.php');
+	exit;
+}
+else
+{
+	require 'library.php';
+	include 'utils/DBlibrary.php';
+	$title="Messaggi: Salone Anna";
+	$title_meta="Messaggi: Salone Anna";
+	$descr="";
+	$keywords="Messaggi, Parrucchiere, Montecchio, Vicenza, Taglio, Colorazioni, Donna";
+	
+	page_start($title, $title_meta, $descr, $keywords,'');
+	$rif='<a href="index.php" xml:lang="en">Home</a> / <strong>Messaggi</strong>';
+	insert_header($rif, 6, true);
+	content_begin();
+	$ris=listamessaggi();
+	if($ris){
+		$str_to_print='<table id="messaggi" summary="Messaggi">
+            <caption>Messaggi</caption>
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Cognome</th>
+                    <th scope="col">Messaggio</th>
+                    <th scope="col">Data</th>
+                    <th scope="col">Ora</th>
+                </tr>
+            </thead>
+            <tbody>';
+		foreach ($ris as $messaggio) {
+			if($messaggio->daLeggere==true)
+				$str_to_print.= "<tr><td><strong>".$messaggio->nome."</strong></td><td><strong>" .$messaggio->cognome."</strong></td><td><strong><a href=\"MostraMessaggio.php?codmsg=".$messaggio->codice." \">".(strlen($messaggio->contenuto)>60? substr($messaggio->contenuto, 0, 60)."...":$messaggio->contenuto)."</a></strong></td><td>".$messaggio->data."</td><td>".$messaggio->ora."</td></tr>";
+			else
+				$str_to_print.= "<tr><td>".$messaggio->nome."</td><td>".$messaggio->cognome."</td><td><a href=\"MostraMessaggio.php?codmsg=".$messaggio->codice." \">".(strlen($messaggio->contenuto)>60? substr($messaggio->contenuto, 0, 60)."...":$messaggio->contenuto)."</a></td><td>".$messaggio->data."</td><td>".$messaggio->ora."</td></tr>";
+		}
+		$str_to_print.="</tbody></table>";
+		unset($messaggio); //fortemente consigliato perché altrimenti l'oggetto $messaggio rimane in memoria
+	}
+	else 
+		$str_to_print="Non sono presenti messaggi";
+	echo $str_to_print;
+	content_end();
+	page_end();
+}
+
+?>
+
---
+++ public_html/MostraMessaggio.php
@@ -0,0 +1,56 @@
+<?php
+
+session_start();
+session_regenerate_id(TRUE);
+
+// Controllo accesso
+if (!isset($_SESSION['username'])) {
+    header('location:index.php');
+    exit;
+} else {
+    require 'library.php';
+    include 'utils/DBlibrary.php';
+    if (!$_GET["codmsg"]) {
+        $err = "Non è stato selezionato alcun messaggio, torna a <a href=\"Messaggi.php\" e riprova";
+    } else {
+        $cmsg = $_GET["codmsg"];
+        $query  = "SELECT c.Nome, c.Cognome, m.Contenuto, m.DataOra, c.Telefono, c.Email FROM Messaggi m JOIN Clienti c WHERE m.CodCliente=c.CodCliente AND m.CodMessaggi='$cmsg'";
+        $conn     = dbconnect();
+        $result   = mysqli_query($conn, $query);
+        // nessun risultato
+        $num_rows = mysqli_num_rows($result);
+        if (!$num_rows)
+            echo "<p>Non è presente il messaggio richiesto</p>";
+        else {
+            $row      = mysqli_fetch_row($result);
+            $to_print = '
+        <ul>
+			<li><p><em>Messaggio inviato da:</em>' . $row[0] . ' ' . $row[1] . '</p></li>
+			<li><p><em>Ricevuto: </em>' . date("d/m/Y H:i", strtotime($row[3])) . '</p></li>
+			<li><p><em>Telefono cliente: </em><a href="' . $row[4] . '"">' . $row[4] . '</a> ; <em xml:lang="en">email:</em> <a href="' . $row[5] . '"">' . $row[5] . '</a></p></li>
+			<li><p><em>Contentuto: </em>' . $row[2] . '</p></li>
+		</ul>
+        ';
+            $qr2      = "UPDATE Messaggi SET ToRead='false' WHERE CodMessaggi='$cmsg'";
+            $result   = mysqli_query($conn, $qr2);
+        }
+    }
+    
+    $title      = "Messaggi: Salone Anna";
+    $title_meta = "Messaggi: Salone Anna";
+    $descr      = "";
+    $keywords   = "Messaggi, Parrucchiere, Montecchio, Vicenza, Taglio, Colorazioni, Donna";
+    
+    page_start($title, $title_meta, $descr, $keywords, '');
+    $rif = '<a href="index.php" xml:lang="en">Home</a> / <a href="Messaggi.php">Messaggi</a> / <strong>Leggi Messaggio</strong>';
+    insert_header($rif, 6, true);
+    content_begin();
+    if (isset($err))
+        echo $err;
+    if (isset($to_print))
+        echo $to_print;
+    content_end();
+    page_end();
+}
+
+?>
