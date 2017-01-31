a3ce0ed1b01a81d44ebd87d4b76b997c8bca6881
pit1988 <gabelli29@hotmail.com>
2017-01-31 16:27:05 +0100

    Tolti loghi in fondo alla pagina

--- public_html/library.php
+++ public_html/library.php
@@ -64,8 +64,8 @@ function page_end() {
 </div>
 <div class="testo-footer, center">
 <p class="imgW3C">
-<a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" \/></a>
-<a href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" \/></a>
+<a href="http://validator.w3.org/check?uri=referer">Documento XHTML 1.0 Valido!</a>
+<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS Valido</a>
 </p>
 </div>
 </div>
