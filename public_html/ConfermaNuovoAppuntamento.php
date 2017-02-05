b4637365feab310f3f5aa5484759a4e414e0516c
pit1988 <gabelli29@hotmail.com>
2017-02-05 17:58:00 +0100

    corretti errori

--- public_html/ConfermaNuovoAppuntamento.php
+++ public_html/ConfermaNuovoAppuntamento.php
@@ -88,7 +88,7 @@ if (!$login) {
                                     <td>".$cliente->telefono."</td>
                                     <td>".$cliente->email."</td>
                                     <td>".$cliente->dataNascita."</td>
-                                    <td class=\"tdin\"><input type='radio' name='CodCliente' value=$cliente->codice></td>
+                                    <td class=\"tdin\"><input type='radio' name='CodCliente' value='$cliente->codice' /></td>
                                 </tr>
                                 ";
                         }
