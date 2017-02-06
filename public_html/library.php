575acd315069ae7bc1a7b1b487471bb54fa23279
pit1988 <gabelli29@hotmail.com>
2017-02-06 13:43:25 +0100

    Minificati files

--- public_html/library.php
+++ public_html/library.php
@@ -20,12 +20,12 @@ function page_start($title, $title_meta, $descr, $keywords, $fun) {
 <meta name="language" content="italian it"/>
 <meta name="viewport" content="width=device-width"/>
 <meta http-equiv="Content-Script-Type" content="application/javascript"/>
-<link rel="stylesheet" href="css/home.css" type="text/css" media="screen and (min-width: 650px)"/>
-<link rel="stylesheet" type="text/css" href="css/print.css" media="print"/>
-<link rel="stylesheet" type="text/css" href="css/small-devices.css" media="screen and (max-width: 650px)"/>
-<!--[if lte IE 8]><link rel="stylesheet" type="text/css" href="css/explorer.css"/><![endif]-->
+<link rel="stylesheet" href="css/home-min.css" type="text/css" media="screen and (min-width: 650px)"/>
+<link rel="stylesheet" type="text/css" href="css/print-min.css" media="print"/>
+<link rel="stylesheet" type="text/css" href="css/small-devices-min.css" media="screen and (max-width: 650px)"/>
+<!--[if lte IE 8]><link rel="stylesheet" type="text/css" href="css/explorer-min.css"/><![endif]-->
 <link rel="icon" href="img/logo2.png" type="image/png"/>
-<script type="text/javascript" src="script/script.js"></script>
+<script type="text/javascript" src="script/script_min.js"></script>
 <script type="text/javascript"  src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 </head>
 ';
